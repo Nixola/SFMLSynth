af606d994cfb6256c1a2d169f783cb2f3896f1b4
Nixola <niki.or97@yahoo.it>
2014-03-17 20:49:13 +0100

    Added some kind of tempo feature, I guess

--- play.lua
+++ play.lua
@@ -7,9 +7,12 @@ local r = f:read '*a'
 
 f:close()
 
+local b = tonumber(r:match("b:(%d+)")) or 1
+
 r = r:gsub("^.-///", "") -- triple slash means "start from here"
 r = r:gsub("//.-\n", "") -- double slash means "skip from here to the end of the line"
 r = r:gsub("*p*", 1000)  -- this allows you to use *p* as pause for clarity, instead of 1000
+
 
 local command = './main'
 local tot = ''
@@ -18,7 +21,7 @@ for i, v in r:gmatch("(%d+) (%d+)") do
 
 	if i == "1000" then i = "-1000" end
 
-	local s = command .. ' ' .. i .. ' ' .. v .. ' ; '
+	local s = command .. ' ' .. i .. ' ' .. v*b .. ' ; '
 	tot = tot .. s
 
 end
